93ff0176743f9dc0ab7ce55340962d571fdca182
Jigar varma <71844973+Jigarvarma2005@users.noreply.github.com>
2021-12-07 19:10:05 +0530

    lol

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ from core.exif import Exif
 
 def main():
     image_path = input("\nEnter image path: ")
-    option = input("\n[1] Get Exif Data\n[2] Destroy Exif Data\n\nChoose an option: ")
+    option = str(input("\n[1] Get Exif Data\n[2] Destroy Exif Data\n\nChoose an option: "))
     exif = Exif()
-    if option == 1:
+    if option == "1":
         exif_data = exif.extract_data(image_path)
         _csv_file = "exif_data.csv"
         if os.path.exists(_csv_file):
@@ -20,7 +20,7 @@ def main():
             for i in exif_data.items():
                 csv_writer.writerow(i)
         print(f"Data saved in '{_csv_file}' ...")
-    elif option == 2:
+    elif option == "2":
         exif.remove_data(image_path)
     else:
         print("\n\n")
